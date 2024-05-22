4bb4bb2df6a4d9c5b4239c9e39cddd6758f6a27e
Bijen11 <shahibijen@gmail.com>
2024-05-22 17:18:47 +0545

    animation to cards added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,43 +1,93 @@
-import Constants from 'expo-constants'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { Dimensions, Image, StyleSheet, Text, View } from 'react-native'
+import Constants from 'expo-constants';
+import {
+  Directions,
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
+import {Dimensions, Image, StyleSheet, Text, View} from 'react-native';
 
+import {Entypo} from '@expo/vector-icons';
+import {StatusBar} from 'expo-status-bar';
 
-import { Entypo } from '@expo/vector-icons'
-import { StatusBar } from 'expo-status-bar'
+import data, {locationImage} from './data';
+import Animated, {
+  Extrapolate,
+  SharedValue,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
-import data, { locationImage } from './data'
+const {width} = Dimensions.get('window');
 
-const { width } = Dimensions.get('window')
-
-const duration = 300
-const _size = width * 0.9
+const duration = 1000;
+const _size = width * 0.9;
 const layout = {
   borderRadius: 16,
   spacing: 12,
   cardsGap: 22,
   width: _size,
   height: _size * 1.27,
-}
-const maxVisibleItems = 6
+};
+const maxVisibleItems = 6;
 
 const colors = {
   primary: '#6667AB',
   light: '#fff',
   dark: '#111',
-}
+};
 
 function Card({
   info,
   index,
   totalLength,
+  activeIndex,
 }: {
-  totalLength: number
-  index: number
-  info: (typeof data)[0]
+  index: number;
+  totalLength: number;
+  info: (typeof data)[0];
+  activeIndex: SharedValue<number>;
 }) {
+  const stylez = useAnimatedStyle(() => {
+    return {
+      position: 'absolute',
+      zIndex: totalLength - index,
+      shadowOpacity: interpolate(
+        activeIndex.value,
+        [index - 1, index, index + 1],
+        [0, 0, 1],
+        {
+          extrapolateRight: Extrapolate.CLAMP,
+        },
+      ),
+      opacity: interpolate(
+        activeIndex.value,
+        [index - 1, index, index + 1],
+        [1 - 1 / maxVisibleItems, 1, 1],
+      ),
+      transform: [
+        {
+          translateY: interpolate(
+            activeIndex.value,
+            [index - 1, index, index + 1],
+            [-layout.cardsGap, 0, layout.height - layout.cardsGap],
+          ),
+        },
+        {
+          scale: interpolate(
+            activeIndex.value,
+            [index - 1, index, index + 1],
+            [0.96, 1, 1],
+          ),
+        },
+      ],
+    };
+  });
+
   return (
-    <View style={[styles.card]}>
+    <Animated.View style={[styles.card, stylez]}>
       <Text
         style={[
           styles.title,
@@ -49,11 +99,10 @@ function Card({
             color: colors.primary,
             opacity: 0.05,
           },
-        ]}
-      >
+        ]}>
         {index}
       </Text>
-      
+
       <View style={styles.cardContent}>
         <Text style={styles.title}>{info.type}</Text>
         <View style={styles.row}>
@@ -62,7 +111,7 @@ function Card({
             {info.from} - {info.to}
           </Text>
         </View>
-        
+
         <View style={styles.row}>
           <Entypo name="location" size={16} style={styles.icon} />
           <Text style={styles.subtitle}>{info.distance} km</Text>
@@ -72,37 +121,64 @@ function Card({
           <Text style={styles.subtitle}>{info.role}</Text>
         </View>
       </View>
-      <Image source={{ uri: locationImage }} style={styles.locationImage} />
-    </View>
-  )
+      <Image source={{uri: locationImage}} style={styles.locationImage} />
+    </Animated.View>
+  );
 }
 
 export default function App() {
+  const activeIndex = useSharedValue(0);
+
+  const flingUp = Gesture.Fling()
+    .direction(Directions.UP)
+    .onStart(() => {
+      if (activeIndex.value === 0) {
+        return;
+      }
+
+      activeIndex.value = withTiming(activeIndex.value - 1, {duration});
+      console.log('fling up');
+    });
+
+  const flingDown = Gesture.Fling()
+    .direction(Directions.DOWN)
+    .onStart(() => {
+      if (activeIndex.value === data.length) {
+        return;
+      }
+
+      activeIndex.value = withTiming(activeIndex.value + 1, {duration});
+      console.log('fling down');
+    });
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <StatusBar hidden />
-      <View
-        style={{
-          alignItems: 'center',
-          flex: 1,
-          justifyContent: 'flex-end',
-          marginBottom: layout.cardsGap * 2,
-        }}
-        pointerEvents="box-none"
-      >
-        {data.slice(0, 1).map((c, index) => {
-          return (
-            <Card
-              info={c}
-              key={c.id}
-              index={index}
-              totalLength={data.length - 1}
-            />
-          )
-        })}
-      </View>
+
+      <GestureDetector gesture={Gesture.Exclusive(flingUp, flingDown)}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            marginBottom: layout.cardsGap * 2,
+          }}
+          pointerEvents="box-none">
+          {data.map((c, index) => {
+            return (
+              <Card
+                info={c}
+                key={c.id}
+                index={index}
+                activeIndex={activeIndex}
+                totalLength={data.length - 1}
+              />
+            );
+          })}
+        </View>
+      </GestureDetector>
     </GestureHandlerRootView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -118,8 +194,16 @@ const styles = StyleSheet.create({
     height: layout.height,
     padding: layout.spacing,
     backgroundColor: colors.light,
+    shadowColor: colors.dark,
+    shadowRadius: 10,
+    shadowOpacity: 1,
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    elevation: 5,
   },
-  title: { fontSize: 32, fontWeight: '600' },
+  title: {fontSize: 32, fontWeight: '600'},
   subtitle: {},
   cardContent: {
     gap: layout.spacing,
@@ -135,4 +219,4 @@ const styles = StyleSheet.create({
     columnGap: layout.spacing / 2,
   },
   icon: {},
-})
+});
